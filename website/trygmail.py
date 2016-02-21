e3afe56ab94f7c23de511a37c2931ce0d7d83f95
TESLA <57leonardo@gmail.com>
2016-02-21 01:44:23 -0500

    getting messages from one person works

--- website/trygmail.py
+++ website/trygmail.py
@@ -9,14 +9,22 @@ messages = ListMessagesMatchingQuery(build_service(get_credentials()), "me", que
 
 #print messages[0]
 
+convos = []
+
 i = 0
-#while (i<len(messages)):
-while(i<1):
-    msg_id = messages[i]["id"]
-    msg_info = GetMessage(build_service(get_credentials()), "me", msg_id)
-    print msg_info["payload"]["headers"][16]["value"]
+while (i<len(messages)):
+    msg_id = messages[i]["threadId"]
+    msg_info = GetMimeMessage(build_service(get_credentials()), "me", msg_id)
+    #print msg_info
+    #TODO: Find a better way to get the message string, ex: by newlines
+    index1 = str(msg_info).index("Content-Type: text/plain; charset=UTF-8")
+    index1 += len("Content-Type: text/plain; charset=UTF-8")
+    index2 = str(msg_info).index("Content-Type: text/html; charset=UTF-8")
+    index2 -= len("--001a113d38862b78c0052be7b244")
+    msg_content = str(msg_info)[index1:index2]
+    convos.append(msg_content)
+    print i
     i+=1
 
-
-
+print convos
 
